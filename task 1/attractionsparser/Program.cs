b1633c8af1b635384af53374f004a23550e7cf66
Katsu <karmaelin8@gmail.com>
2022-01-20 21:53:08 +0300

    attractions parser

---
+++ task 1/attractionsparser/Program.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+
+string url = "https://avolgograd.com/sights?obl=vgg";
+
+var web = new HtmlWeb();
+var doc = web.Load(url);
+
+var nodes = doc.DocumentNode.SelectNodes("//div[@id='afisha-content']//a[@href]");
+
+List<string> attractions = new List<string>();
+
+foreach (var node in nodes)
+{
+    if(!string.IsNullOrEmpty(node.InnerText))
+        attractions.Add(node.InnerText);
+}
+
+List<string> lines = new();
+
+foreach (var item in attractions)
+{
+    string[] line =
+    {
+        $"PersonsNames \"{item.Replace(" ", "_")}\"",
+        "{",
+        $"\tkey = \"{item}\"",
+        $"\tlemma = \"{item}\"",
+        "}",
+        " "
+    };
+    foreach (var items in line)
+    {
+        lines.Add(items);
+    }
+}
+
+File.WriteAllLines("WriteLines.txt", lines.ToArray());
